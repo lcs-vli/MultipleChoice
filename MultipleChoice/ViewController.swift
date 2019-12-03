6c9dcec04cf84f06887cf05b847e050757a44ef7
Victoria Li <VLi@lcs.on.ca>
2019-12-03 14:59:06 -0500

    Initial Commit

---
+++ MultipleChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleChoice
+//
+//  Created by Li, Muchen on 2019-12-03.
+//  Copyright © 2019 Li, Muchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
